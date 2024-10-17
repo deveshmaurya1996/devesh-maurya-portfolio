60f3d826170f8397fd0b159f4f6530e9b9719bc8
unknown <deveshmaurya1996@gmail.com>
2024-10-17 13:26:24 +0530

    removed-copyright

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -9,7 +9,6 @@ const Footer = () => {
     <footer className="w-full bg-gray-50 py-6">
       <div className="flex items-center justify-center gap-1">
         <Typography className="flex items-center" variant="body3">
-          <Copyright className="mr-1 inline-block h-4 w-4" />
           {new Date().getFullYear()} |&nbsp; &nbsp;and&nbsp;
           <Link
             noCustomization
